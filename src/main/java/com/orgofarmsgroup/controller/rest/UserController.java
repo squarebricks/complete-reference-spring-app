de020b002dcf4cbf75d5d534abb3aeeb2459e5a9
Ramakrishna Janapureddy <85489242+codecoder005@users.noreply.github.com>
2023-03-28 12:25:08 +0530

    cross origin removed

--- src/main/java/com/orgofarmsgroup/controller/rest/UserController.java
+++ src/main/java/com/orgofarmsgroup/controller/rest/UserController.java
@@ -7,13 +7,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/users")
 @RequiredArgsConstructor
 @Slf4j
