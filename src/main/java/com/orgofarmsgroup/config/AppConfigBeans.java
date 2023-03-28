f8574c527bca43255e4065f12ec844cdc5f34450
Ramakrishna Janapureddy <85489242+codecoder005@users.noreply.github.com>
2023-03-28 11:03:31 +0530

    internationalization and cors added (#14)
    * lot of changes
    
    * redis added
    
    * internationalization and cors added

--- src/main/java/com/orgofarmsgroup/config/AppConfigBeans.java
+++ src/main/java/com/orgofarmsgroup/config/AppConfigBeans.java
@@ -3,11 +3,24 @@ package com.orgofarmsgroup.config;
 import com.google.gson.Gson;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class AppConfigBeans {
+    @Value("${REDIS_HOST:localhost}")
+    private String redisHost;
+    @Value("${REDIS_PORT:6379}")
+    private Integer redisPort;
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
@@ -17,5 +30,25 @@ public class AppConfigBeans {
     @Bean
     public Gson jsonHelper() {
         return new Gson();
+    }
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 }
